82e838b9a72ce45765658cd5a3014380b4a6d0c5
Tobias Casper <tobias.casper@gmail.com>
2019-05-07 18:53:30 +0200

    Allow giving dir paths to local_path in DownloadFile operation

--- lib/sftp_client/operations/download_file.ex
+++ lib/sftp_client/operations/download_file.ex
@@ -1,12 +1,22 @@
 defmodule SFTPClient.Operations.DownloadFile do
   use SFTPClient.Operation
 
+  @spec download_file!(Conn.t(), String.t(), String.t()) :: :ok | no_return
   def download_file!(%Conn{} = conn, remote_path, local_path) do
+    local_path = get_local_path(local_path, remote_path)
     source_stream = SFTPClient.stream_file(conn, remote_path)
     target_stream = File.stream!(local_path)
 
     source_stream
     |> Stream.into(target_stream)
     |> Stream.run()
+  end
+
+  defp get_local_path(local_path, remote_path) do
+    if File.dir?(local_path) do
+      Path.join(local_path, Path.basename(remote_path))
+    else
+      local_path
+    end
   end
 end

8b34a60218928dd26541b4510a8c625c8d0aa5f6
Tobias Casper <tobias.casper@gmail.com>
2020-04-30 11:42:57 +0200

    chore: upgrade some deps

--- mix.exs
+++ mix.exs
@@ -75,11 +75,11 @@ defmodule SFTPClient.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.0.5", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.11.0", only: :test},
-      {:ex_doc, "~> 0.20.2", only: :dev},
-      {:mox, "~> 0.5.0", only: :test},
+      {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.11", only: :test},
+      {:ex_doc, "~> 0.20", only: :dev},
+      {:mox, "~> 0.5", only: :test},
       {:temp, "~> 0.4", only: :test}
     ]
   end
